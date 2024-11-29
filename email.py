29d675a16c718ecd126ed91f93e21b9d33a4bbfb
MikeKent961 <mk961@kent.ac.uk>
2024-11-29 11:39:24 +0000

    did postcode and most of the email one where it recognises what email you put in but not what the errors could be if you get it wrong

--- email.py
+++ email.py
@@ -1,1 +1,14 @@
+import re
 
+email = input("Enter email: ").strip()
+
+if re.search(r"^\w+@\w.+\.(ac.uk|gov.uk|nhs.net)$", email):
+    if re.search(".ac.uk", email):
+        print("Valid Academic Email.")
+    elif re.search(".gov.uk", email):
+        print("Valid Government Email.")
+    elif re.search(".nhs.net", email):
+        print("Valid NHS Email.")
+
+else:
+    print("Not Valid")
--- postcode.py
+++ postcode.py
@@ -1,1 +1,9 @@
+import re
 
+address = input("Insert Address somehow: ")
+postcode = re.search(r"[A-Z]{1,2}+\d+[A-Z0-9]?\s?+[0-9]+[A-Z]{2}", address)
+
+if postcode:
+    print(postcode.group())
+else:
+    print("No postcode")
